448b6aabeffdae4028543096a4c4c617c459aa5c
Tobias Lundell <tobias.lundell@ericsson.com>
2019-12-12 21:17:12 +0100

    Add solution for day 3 - part 2

--- day_3/crossedWires.js
+++ day_3/crossedWires.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { inRange, range, intersection } = require("lodash");
+const { inRange, range, intersection, findIndex } = require("lodash");
 
 const inputFilePath = path.resolve(__dirname, process.argv[2] || "wires.txt");
 
@@ -16,7 +16,7 @@ const yDirections = ["U", "D"];
 const isNegativeDirection = direction =>
   [xDirections[1], yDirections[1]].includes(direction);
 
-const generateLine = (x, y, path) => {
+const generateLine = (x, y, accSteps, path) => {
   const [direction, ...steps] = path;
   const noOfSteps = +steps.join("");
 
@@ -26,13 +26,15 @@ const generateLine = (x, y, path) => {
       x: isNegativeDirection(direction)
         ? [x, x - noOfSteps]
         : [x, x + noOfSteps],
-      y
+      y,
+      accSteps: noOfSteps + accSteps
     };
   }
   // Vertical line (y-direction)
   return {
     x,
-    y: isNegativeDirection(direction) ? [y, y - noOfSteps] : [y, y + noOfSteps]
+    y: isNegativeDirection(direction) ? [y, y - noOfSteps] : [y, y + noOfSteps],
+    accSteps: noOfSteps + accSteps
   };
 };
 
@@ -41,11 +43,12 @@ const generateLines = (start, paths) => {
   const lines = [start];
   paths.forEach(path => {
     // End coordinates of previous line
-    const { x, y } = lines.slice(-1)[0];
+    const { x, y, accSteps } = lines.slice(-1)[0];
     lines.push(
       generateLine(
         x.length ? x.slice(-1)[0] : x,
         y.length ? y.slice(-1)[0] : y,
+        accSteps,
         path
       )
     );
@@ -53,38 +56,30 @@ const generateLines = (start, paths) => {
   return lines;
 };
 
-const findClosestPerpendicularIntersection = (ref, comp) => {
-  const intersections = [];
+const findPerpendicularIntersections = (ref, comp) => {
   if (isHorizontal(ref)) {
     // Ref line is horizontal, find intersections with vertical lines
-    const xIntersections = comp
+    return comp
       .filter(
         line =>
           (inRange(line.x, ...ref.x) || ref.x[1] === line.x) &&
           (inRange(ref.y, ...line.y) || line.y[1] === ref.y)
       )
-      .map(line => line.x);
-    if (xIntersections.length) {
-      intersections.push([Math.min(...xIntersections.map(Math.abs)), ref.y]);
-    }
+      .map(line => [line.x, ref.y]);
   } else if (isVertical(ref)) {
     // Ref line is vertical, find intersections with horizontal lines
-    const yIntersections = comp
+    return comp
       .filter(
         line =>
           (inRange(ref.x, ...line.x) || line.x[1] === ref.x) &&
           (inRange(line.y, ...ref.y) || ref.y[1] === line.y)
       )
-      .map(line => line.y);
-    if (yIntersections.length) {
-      intersections.push([ref.x, Math.min(...yIntersections.map(Math.abs))]);
-    }
+      .map(line => [ref.x, line.y]);
   }
-  return intersections;
+  return [];
 };
 
-const findClosestParallelOverlaps = (ref, comp) => {
-  const overlaps = [];
+const findParallelOverlaps = (ref, comp) => {
   if (isHorizontal(ref)) {
     // Ref line is horizontal, find overlaps with other horizontal lines
     const xOverlaps = comp
@@ -100,9 +95,10 @@ const findClosestParallelOverlaps = (ref, comp) => {
     if (xOverlaps.length) {
       const refRange = range(...ref.x);
       const commonX = intersection(refRange, ...xOverlaps);
-      const minCommonX = Math.min(...commonX.map(Math.abs));
       // Do not count starting point as intersection
-      !(minCommonX === 0 && ref.y === 0) && overlaps.push([minCommonX, ref.y]);
+      return commonX
+        .filter(x => !(x === 0 && ref.y === 0))
+        .map(x => [x, ref.y]);
     }
   } else if (isVertical(ref)) {
     // Ref line is vertical, find overlaps with other vertical lines
@@ -119,54 +115,108 @@ const findClosestParallelOverlaps = (ref, comp) => {
     if (yOverlaps.length) {
       const refRange = range(...ref.y);
       const commonY = intersection(refRange, ...yOverlaps);
-      const minCommonY = Math.min(...commonY.map(Math.abs));
       // Do not count starting point as intersection
-      !(minCommonY === 0 && ref.x === 0) && overlaps.push([ref.x, minCommonY]);
+      return commonY
+        .filter(y => !(y === 0 && ref.x === 0))
+        .map(y => [ref.x, y]);
     }
   }
-  return overlaps;
+  return [];
 };
 
-const findClosestIntersectionDistance = (wire1, wire2) => {
+const findAllIntersectionPoints = (wire1, wire2) => {
   const intersections = [];
   wire1.horizontal.forEach(line => {
     intersections.push(
-      ...findClosestPerpendicularIntersection(line, wire2.vertical),
-      ...findClosestParallelOverlaps(line, wire2.horizontal)
+      ...findPerpendicularIntersections(line, wire2.vertical),
+      ...findParallelOverlaps(line, wire2.horizontal)
     );
   });
   wire1.vertical.forEach(line => {
     intersections.push(
-      ...findClosestPerpendicularIntersection(line, wire2.horizontal),
-      ...findClosestParallelOverlaps(line, wire2.vertical)
+      ...findPerpendicularIntersections(line, wire2.horizontal),
+      ...findParallelOverlaps(line, wire2.vertical)
     );
   });
-  return Math.min(...intersections.map(([x, y]) => Math.abs(x) + Math.abs(y)));
+  return intersections;
+};
+
+const getFewestStepsToIntersection = (
+  intersections,
+  wire1Lines,
+  wire2Lines
+) => {
+  const isXYOnLine = (x, y) => line =>
+    line.x.length
+      ? y === line.y && (inRange(x, ...line.x) || line.x[1] === x)
+      : x === line.x && (inRange(y, ...line.y) || line.y[1] === y);
+
+  const stepsFromEndOfLine = (line, x, y) =>
+    line.x.length ? Math.abs(line.x[1] - x) : Math.abs(line.y[1] - y);
+  // TODO: Find first index of lines including intersection points
+  return intersections.reduce((minSteps, [x, y]) => {
+    const lineIncludesXY = isXYOnLine(x, y);
+    const wire1FirstCrossingIndex = findIndex(wire1Lines, lineIncludesXY);
+    const wire2FirstCrossingIndex = findIndex(wire2Lines, lineIncludesXY);
+
+    const steps =
+      wire1Lines[wire1FirstCrossingIndex].accSteps -
+      stepsFromEndOfLine(wire1Lines[wire1FirstCrossingIndex], x, y) +
+      wire2Lines[wire2FirstCrossingIndex].accSteps -
+      stepsFromEndOfLine(wire2Lines[wire2FirstCrossingIndex], x, y);
+    if (minSteps > 0) return Math.min(minSteps, steps);
+
+    return steps;
+  });
 };
 
 const centerPos = {
   x: 0,
-  y: 0
+  y: 0,
+  accSteps: 0
 };
 
 let wire1Lines = generateLines(centerPos, wire1Path);
 wire1Lines = {
+  ordered: wire1Lines,
   horizontal: wire1Lines.filter(isHorizontal),
   vertical: wire1Lines.filter(isVertical)
 };
 
 let wire2Lines = generateLines(centerPos, wire2Path);
 wire2Lines = {
+  ordered: wire2Lines,
   horizontal: wire2Lines.filter(isHorizontal),
   vertical: wire2Lines.filter(isVertical)
 };
 
-const closestCrossingDistance = findClosestIntersectionDistance(
-  wire1Lines,
-  wire2Lines
+const intersections = findAllIntersectionPoints(wire1Lines, wire2Lines);
+
+/**
+ * Part 1
+ */
+const closestCrossingDistance = Math.min(
+  ...intersections.map(([x, y]) => Math.abs(x) + Math.abs(y))
 );
 
 console.log(
   "Manhattan distance to closest intersection:",
   closestCrossingDistance
 );
+/**
+ * END
+ */
+
+/**
+ * Part 2
+ */
+const fewestSteps = getFewestStepsToIntersection(
+  intersections,
+  wire1Lines.ordered,
+  wire2Lines.ordered
+);
+
+console.log("Fewest steps to intersection:", fewestSteps);
+/**
+ * END
+ */
