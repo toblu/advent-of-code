f1c6d2b91c5467c5cd732e1628b7de4a3574fc1d
Tobias Lundell <tobias.lundell@ericsson.com>
2019-12-15 21:40:30 +0100

    Remove test data

--- day_6/orbitMap.js
+++ day_6/orbitMap.js
@@ -6,9 +6,6 @@ let YOU;
 let SAN;
 
 const orbits = readFileInput(process.argv[2]).split("\n");
-// const orbits = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN".split(
-//   "\n"
-// );
 
 const findOrbitingObjects = object =>
   orbits
